d0ae2e67540b76f7eeb27b79e7ff5a77ee6e38eb
rakeshrkhub <rakeshkumarrambad@gmail.com>
2022-04-29 18:01:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+
+ window.onscroll = () =>{
+
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+  }
+
+  window.onload = () =>{
+
+    if(window.scrollY > 80){
+      document.querySelector('.header .header-2').classList.add('active');
+    }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+    }
+  
+  }
+
+  var swiper = new Swiper(".product-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
